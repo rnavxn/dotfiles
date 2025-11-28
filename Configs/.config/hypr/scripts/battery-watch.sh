a3ea3778657e66d315e1d4f3d701fc83e7a9da47
Navin <rnavxn@gmail.com>
2025-11-28 17:43:45 +0530

    hypr: add battery full notifier alonglisde existing battery-watch.sh script with modified thresholds

--- Configs/.config/hypr/scripts/battery-watch.sh
+++ Configs/.config/hypr/scripts/battery-watch.sh
@@ -1,20 +1,26 @@
-
 #!/bin/bash
 
-# Set battery threshold
-THRESHOLD=80
-CHECK_INTERVAL=60   # seconds
+LOW_THRESHOLD=19
+FULL_THRESHOLD=79       # I use 79 here because I personally user a 80% battery cap.
+CHECK_INTERVAL=60
+
+ICON_LOW="/usr/share/icons/char-white/status/48/battery-caution.svg"
+ICON_FULL="=/usr/share/icons/hicolor/128x128/status/battery-full.png"
 
 while true; do
-    # Read battery percentage
     PERC=$(cat /sys/class/power_supply/BAT*/capacity)
-
-    # Read charging status
     STATUS=$(cat /sys/class/power_supply/BAT*/status)
 
-    # Trigger only if NOT charging and below threshold
-    if [ "$STATUS" != "Charging" ] && [ $PERC -le $THRESHOLD ]; then
-        notify-send -u critical -i "battery-caution" "LOW BATTERY" "Battery at $PERC% — plug in now!"
+    # Low battery warning
+    if [ "$STATUS" != "Charging" ] && [ $PERC -le $LOW_THRESHOLD ]; then
+        notify-send -u critical -i "$ICON_LOW" \
+        "Low Battery" "Battery is at ${PERC}%. Plug in now."
+    fi
+
+    # Battery full reminder when charging
+    if [ "$STATUS" == "Charging" ] && [ $PERC -ge $FULL_THRESHOLD ]; then
+        notify-send -u normal -i "$ICON_FULL" \
+        "Battery Charged" "Battery is at ${PERC}%. You can unplug."
     fi
 
     sleep $CHECK_INTERVAL
